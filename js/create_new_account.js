2ffb16d2e9ba84a1de869e13c872b4e8f35135df
Mezan2002 <mezanurr009@gmail.com>
2022-08-15 20:05:18 +0600

    Create New Account page

---
+++ js/create_new_account.js
@@ -0,0 +1,8 @@
+document.getElementById("password").addEventListener("keyup", function (event) {
+  const password = event.target.value;
+});
+document
+  .getElementById("confirmPassword")
+  .addEventListener("keyup", function (event) {
+    const confirmPassword = event.target.value;
+  });
